63485f95a8e846de31b7aa03b1a9712e3c843736
Jessica Minnier <jessica.minnier@gmail.com>
2018-02-01 13:07:37 -0800

    clean up code

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,12 +1,10 @@
 library(tidyverse)
-library(janitor)
 library(here)
 
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
 
-ip <- installed.packages() %>% as_tibble()
 
 ## keep the variables
 ##   * Package
@@ -15,14 +13,17 @@ ip <- installed.packages() %>% as_tibble()
 ##   * Priority
 ##   * Built
 
-ip <- ip %>% select(Package:Priority, Built)
-
 ## write it to data/installed-packages.csv
-write_csv(ip,path=here("data","installed-packages.csv"))
 
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+
+ip <- installed.packages() %>%
+  as_tibble() %>%
+  select(Package:Priority, Built) %>%
+  write_csv(path=here("data","installed-packages.csv"))
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
